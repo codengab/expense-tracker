e7521e3ff1e9884f3e2b1bf00a7c14e454d67a68
nurhadinubi <nurhadunubi@gmail.com>
2026-01-02 16:37:05 +0700

    Adjust transaction form fields to use dropdowns

--- app/Filament/Resources/Transactions/Schemas/TransactionForm.php
+++ app/Filament/Resources/Transactions/Schemas/TransactionForm.php
@@ -3,30 +3,55 @@
 namespace App\Filament\Resources\Transactions\Schemas;
 
 use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
+use Illuminate\Database\Eloquent\Builder;
 
 class TransactionForm
 {
-    public static function configure(Schema $schema): Schema
-    {
-        return $schema
-            ->components([
-                TextInput::make('workspace_id')
-                    ->required()
-                    ->numeric(),
-                TextInput::make('wallet_id')
-                    ->required()
-                    ->numeric(),
-                TextInput::make('category_id')
-                    ->required()
-                    ->numeric(),
-                TextInput::make('amount')
-                    ->required()
-                    ->numeric(),
-                DatePicker::make('transaction_date')
-                    ->required(),
-                TextInput::make('description'),
-            ]);
-    }
+  public static function configure(Schema $schema): Schema
+  {
+    return $schema->schema([
+      Select::make('wallet_id')
+        ->label('Wallet')
+        ->relationship(
+          name: 'wallet',
+          titleAttribute: 'name',
+          modifyQueryUsing: function (Builder $query) {
+            $query->where(
+              'workspace_id',
+              auth()->user()->current_workspace_id
+            );
+          }
+        )
+        ->searchable()->required(),
+
+      Select::make('category_id')
+        ->label('Category')
+        ->relationship(
+          name: 'category',
+          titleAttribute: 'name',
+          modifyQueryUsing: function (Builder $query) {
+            $query->where(
+              'workspace_id',
+              auth()->user()->current_workspace_id
+            );
+          }
+        )->searchable()->required(),
+
+        
+      TextInput::make('amount')
+        ->numeric()
+        ->required(),
+
+      DatePicker::make('transaction_date')
+        ->default(now())
+        ->required(),
+
+      Textarea::make('description')
+        ->columnSpanFull(),
+    ]);
+  }
 }
